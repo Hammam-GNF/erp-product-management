02f2c4fa9b537df62e49c86f87118edbfa675d9f
Hammam GNF <hammamgonjil@gmail.com>
2026-02-13 19:08:09 +0700

    refactor: improve product validation and input sanitization

--- models/Product.php
+++ models/Product.php
@@ -35,8 +35,18 @@ class Product
 
     public function create(array $data): bool
     {
-        if ($data['stock'] < 0) {
-            throw new Exception("Stock cannot be negative");
+        if (
+            !isset($data['category_id'], $data['name_product'], $data['price'], $data['stock'])
+        ) {
+            throw new Exception("Incomplete data");
+        }
+
+        if (!is_numeric($data['stock']) || $data['stock'] < 0) {
+            throw new Exception("Stock must be a non-negative number");
+        }
+
+        if (!is_numeric($data['price']) || $data['price'] < 0) {
+            throw new Exception("Price must be a non-negative number");
         }
 
         $query = "
