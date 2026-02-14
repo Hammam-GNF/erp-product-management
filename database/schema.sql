a312170ad7684bdade412d09f4abc5c9d7335da0
Hammam GNF <hammamgonjil@gmail.com>
2026-02-14 15:29:43 +0700

    feat(database): add database creation and dummy seed data

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS erp_product_management;
+USE erp_product_management;
+
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name_category VARCHAR(100) NOT NULL
@@ -15,3 +18,14 @@ CREATE TABLE products (
         REFERENCES categories(id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
+
+INSERT INTO categories (name_category) VALUES
+('Elektronik'),
+('Pakaian Pria'),
+('Pakaian Wanita');
+
+INSERT INTO products (category_id, name_product, price, stock) VALUES
+(1, 'Smartphone', 5000000, 10),
+(1, 'Laptop', 8000000, 5),
+(2, 'Baju Pria', 200000, 5),
+(3, 'Baju Wanita', 150000, 8);
